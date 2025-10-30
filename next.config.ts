7a0142582366ffbfcfc3f1ab5d939c8b263952ba
Ndulkis <ndulkis@gmail.com>
2025-10-30 11:05:14 -0700

    deleted: workflow to github pages

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,7 @@
 import type { NextConfig } from "next";
 
-const repo = 'buck-et_list_brands';
-const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig: NextConfig = {
-  basePath: isProd ? `/${repo}` : undefined,
-  assetPrefix: isProd ? `/${repo}/` : undefined,
-  images: { unoptimized: true }, // needed if you’re exporting statically
+ 
 };
 
 
